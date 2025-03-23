7ea3026548cc066ed6efbc6c8850a3f718d0a69c
pash masr <129723189+pashamasr01287654800@users.noreply.github.com>
2025-03-23 08:35:02 +0200

    Update macchanger_eth0.sh

--- macchanger_eth0.sh
+++ macchanger_eth0.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 # Disable the network interface
-ifconfig eth0 down
+sudo ifconfig eth0 down
 
 # Change the MAC address
-macchanger -a eth0
+sudo macchanger -a eth0
 
 # Enable the network interface
-ifconfig eth0 up
-
-# Check if 'service' command is available and restart the network manager accordingly
-if command -v service &> /dev/null; then
-    systemctl restart NetworkManager
-else
-    service network-manager restart
-fi
+sudo ifconfig eth0 up
