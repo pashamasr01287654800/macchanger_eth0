7c3a0501fb6306d490fe42430b0045523d5ec9c1
pash masr <129723189+pashamasr01287654800@users.noreply.github.com>
2025-03-27 03:10:13 +0200

    Update macchanger_eth0.sh

--- macchanger_eth0.sh
+++ macchanger_eth0.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Disable the network interface
-sudo ifconfig eth0 down
+ifconfig eth0 down
 
 # Change the MAC address
-sudo macchanger -a eth0
+macchanger -a eth0
 
 # Enable the network interface
-sudo ifconfig eth0 up
+ifconfig eth0 up
